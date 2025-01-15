5b423d79d30c61f0cf8bc48f757485837f4506f5
endtrastic <a.tuvikene@voco.ee>
2025-01-15 09:49:36 +0200

    The base files that I'm working with

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+
+
+    return (
+        <header>
+            <h1>Demo Webiste</h1>
+            <button>Toggle Theme</button>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Page.jsx
@@ -0,0 +1,22 @@
+import Header from './Header';
+
+
+const Page = () => {
+
+
+    return (
+        <div id="app">
+            <Header/>
+
+            <article>
+                <h2>React Course</h2>
+                <p>
+                    A course that is about React.
+                </p>
+            </article>
+        </div>
+    );
+}
+
+
+export default Page
